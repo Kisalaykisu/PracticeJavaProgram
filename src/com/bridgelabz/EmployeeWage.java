7008ebe9bc5ee600269b04642d8a6425004af9a2
Kisalay <kisalaykisu@gmail.com>
2022-06-18 13:35:51 +0530

    files added:

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+    public static void main(String args[]) {
+        //Welcome Message.
+        System.out.println("Welcome into the playground of coding,Line comparison computation is the first one.");
+    }
+}
