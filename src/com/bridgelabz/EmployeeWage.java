f8d3636e52be4713ada843494e4999181cddeee7
Kisalay <kisalaykisu@gmail.com>
2022-06-18 14:08:02 +0530

    File3 has been added:

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,12 +3,19 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String args[]) {
 
+        double IS_FULL_TIME=1;
+        double IS_PART_TIME=2;
         int WAGE_PER_HR = 20;
-        int WORKING_HRS = 8;
+        int empHrs = 0;
 
-        int dailyWage=WAGE_PER_HR*WORKING_HRS;
-        System.out.println("Daily wage of Employee is: " + dailyWage);
+        double empCheck = Math.floor(Math.random()* 10) % 3;
+        if(empCheck==IS_FULL_TIME){
+            empHrs = 8;
+        }else if (empCheck==IS_PART_TIME)
+            empHrs = 4;
+
+        int empWage = empHrs * WAGE_PER_HR;
+        System.out.println("Employee wage is: " + empWage);
 
     }
-
 }
