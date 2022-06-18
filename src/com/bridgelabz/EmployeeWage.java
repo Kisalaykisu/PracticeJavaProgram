7c667bc10c4caae2040687413bb09ba9c3ff5029
Kisalay <kisalaykisu@gmail.com>
2022-06-18 14:29:07 +0530

    File 7 has been added:

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,17 +1,23 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static void main(String args[]) {
 
-        final int IS_FULL_TIME = 1;
-        final int IS_PART_TIME=2;
-        final int WAGE_PER_HR = 20;
-        int empHrs = 0;
-        int monthlyWage = 0;
-        int totalWorkingHrs = 100;
-        int workedHrs=0;
-        int i;
+    static  final int IS_FULL_TIME = 1;
+    static  final int IS_PART_TIME=2;
+    static final int WAGE_PER_HR = 20;
+    static int empHrs = 0;
+    static int monthlyWage = 0;
+    static int totalWorkingHrs = 100;
+    static int workedHrs=0;
+    static int i;
 
+    public static void main(String[] args){
+
+        employeeWage();
+
+
+    }
+    public static void employeeWage(){
         for ( i=0;i<20 && workedHrs<totalWorkingHrs;i++) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -26,4 +32,5 @@ public class EmployeeWage {
         System.out.println("Employee Monthly wage is: " + monthlyWage);
         System.out.println("Working Days = " + i + ", Total working hrs = " + workedHrs);
     }
+
 }
