4f59121d62f8272eaae43e7d29579c1fc38bce5a
Kisalay <kisalaykisu@gmail.com>
2022-06-23 19:06:29 +0530

    UC8 has been added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,33 +1,50 @@
-/*Refactor the Code to write a Class Method to Compute Employee Wage
-- Use Class Method and Class Variables.
+/*Compute Employee Wage for multiple companies
+ - Note: Each Company has its own wage, number of working days and working hours per month
+- Use Class Method with function parameters instead of Class Variables
 */
 
 package com.bridgelabz;
 
-public class EmployeeWage {
+import java.util.Scanner;
 
-    static  final int IS_FULL_TIME = 1;
-    static  final int IS_PART_TIME=2;
-    static final int WAGE_PER_HR = 20;
-    static int empHrs = 0;
-    static int monthlyWage = 0;
-    static int totalWorkingHrs = 100;
-    static int workedHrs=0;
+public class EmployeeWage {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME=2;
+    int wagePerHr;
+    int empHrs = 0;
+    int monthlyWage = 0;
+    int totalWorkingHrs;
+    int totalWorkingDays;
+    int workedHrs=0;
     static int i;
 
     public static void main(String[] args){
 
-        employeeWage();
+        EmployeeWage company1 = new EmployeeWage();
+        EmployeeWage company2 = new EmployeeWage();
+
+        System.out.println("Employee wage for company1: ");
+        company1.employeeWage();
+        System.out.println("Employee wage for company2: ");
+        company2.employeeWage();
 
     }
-    public static void employeeWage(){
-        for ( i=0;i<20 && workedHrs<totalWorkingHrs;i++) {
+    public void employeeWage(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the wage_per_hr: ");
+        wagePerHr = scanner.nextInt();
+        System.out.println("Enter the total Working days: ");
+        totalWorkingDays = scanner.nextInt();
+        System.out.println("Enter the total working hrs in a month: ");
+        totalWorkingHrs = scanner.nextInt();
+
+        for ( i=0;i<totalWorkingDays && workedHrs<totalWorkingHrs;i++) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME -> empHrs = 8;
                 case IS_PART_TIME -> empHrs = 4;
             }
-            int empWage = empHrs * WAGE_PER_HR;
+            int empWage = empHrs * wagePerHr;
             System.out.println("Employee wage is: " + empWage);
             monthlyWage+=empWage;
             workedHrs+=empHrs;
